940d0ec1fed75247cc9ddbace1549972c99e6dae
Steve Palacios <steve@teamrampage.net>
2019-11-04 12:01:00 -0800

    Added the connection scripts for a couple Linode servers

---
+++ alderaan.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "---------"
+echo "Connecting to Alderaan at Linode"
+echo "---------"
+
+sleep 1
+ssh kingsolmn@173.255.216.115
---
+++ tatooine.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "---------"
+echo "Connecting to Tatooine at Linode"
+echo "---------"
+
+sleep 1
+ssh kingsolmn@96.126.104.196
